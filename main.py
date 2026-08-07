79e5caa92abe9fb5542d1df6a87a6312c02d5865
pishtov <eupishtov@gmail.com>
2026-08-07 23:00:12 +0300

    fix: Adds positive result of port scan

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import socket
 
 ip = "127.0.0.1"
-port = 80
+port = 8080
 
 try:
     sock = socket.socket()
     sock.connect((ip, port))
+    print(f"Port {port} is OPEN")
 except Exception as e:
     print(f"Error occurred: {e}")
